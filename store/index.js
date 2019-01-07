53dcc6cbd6730d054567b069f177773f7d7c64ed
first <firstnaruto@msn.com>
2019-01-07 13:59:59 +0700

    [🔥] Remove import file.

--- store/index.js
+++ store/index.js
@@ -2,7 +2,6 @@ import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import reducers from '../reducers'
 import { createLogger } from 'redux-logger'
-import * as todoAction from '../actions/todo'
 
 const middleware = [thunk]
 if (process.env.NODE_ENV !== 'production') {
